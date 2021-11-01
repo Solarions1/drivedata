0e861401344c2e365ffc37b4fdf73785066ebe43
Solarions1 <manu.herrerap1@gmail.com>
2021-11-01 18:54:14 -0500

    Adding new pages

--- public/js/movieser.js
+++ public/js/movieser.js
@@ -1,18 +1,34 @@
 function showplay(){
   var x = document.getElementById('idbus').value;
-  var div1 = document.getElementById('morekd');
-  var div2 = document.getElementById('ringplay');
-  var div3 = document.getElementById('titan');
+  var data1 = "Titanic";
+  var data1p1= "titanic";
 
-  if (x == div1){
-    div1.style.dysplay = "block";
+  var data2 = "The ring";
+  var data2p2 = "The Ring";
+  var data2p3 = "The ring 3";
+  var data2p4 = "The Ring 3";
+  var data2p5 = "El aro";
+  if( x == data1 | data1p1){
+    document.getElementById('name').innerHTML="Name: Titanic";
+    document.getElementById('aval').innerHTML="Available: True";
+    document.getElementById('qual').innerHTML="HD: True 1080p";
+    document.getElementById('lan').innerHTML="Lang: ESP LAA | EN EUA";
   }
+  if( x == data2 | data2p2 |data2p3 | data2p4 | data2p5){
+    document.getElementById('name').innerHTML="Name: The Ring";
+    document.getElementById('aval').innerHTML="Available: True";
+    document.getElementById('qual').innerHTML="HD: True 1080p";
+    document.getElementById('lan').innerHTML="Lang: ESP LAA | EN EUA";
+  }
+  
+  
+}
 
-  if (x == div2){
-    div2.style.dysplay = "block";
-  }
-
-  if (x == div3){
-    div3.style.dysplay = "block";
-  }
+function clearcon() {
+  // body...
+    document.getElementById('name').innerHTML="";
+    document.getElementById('aval').innerHTML="";
+    document.getElementById('qual').innerHTML="";
+    document.getElementById('lan').innerHTML="";
+  
 }
