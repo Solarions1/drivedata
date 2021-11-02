4d8957287c0a964a18cbbb75e0cf3212fa0dc221
Solarions1 <manu.herrerap1@gmail.com>
2021-11-02 12:26:47 -0500

    Update variable data user

--- public/js/movieser.js
+++ public/js/movieser.js
@@ -8,6 +8,7 @@ function showplay(){
   var data2p3 = "The ring 3";
   var data2p4 = "The Ring 3";
   var data2p5 = "El aro";
+  var datapriv = "priv1102";
   if( x == data1 | data1p1){
     document.getElementById('name').innerHTML="Name: Titanic";
     document.getElementById('aval').innerHTML="Available: True";
@@ -20,7 +21,13 @@ function showplay(){
     document.getElementById('qual').innerHTML="HD: True 1080p";
     document.getElementById('lan').innerHTML="Lang: ESP LAA | EN EUA";
   }
+
+  if(x == datapriv){
+    document.getElementById('name').innerHTML="Redirect to test main page in 5 seconds";
+    setTimeout(() => window.location.href="https://solarions1.github.io/drivedata/public/pages/directory/ntvnD2UHkUz6YZHqA3Je79h4uFptkXrFagKZSidni2BTDuDdVLgmJ7G92Z2TVeaBuGtCXJRU9ntvnD2UHkUz6YZHqA3Je79h4uFptkXrFagKZSidni2BTDuDdVLgmJ7G92Z2TVe.html", 5000);
+  }
   
+
   
 }
 
